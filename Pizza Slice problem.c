d41ce6d0bddf99d51988d1f6fc570d87158a516d
roni-2004 <jaronronialbert@gmail.com>
2022-09-23 19:35:13 +0530

    Pizza Slice

---
+++ Pizza Slice problem.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int angle1,angle2,angle3;
+    scanf("%d%d%d",&angle1,&angle2,&angle3);
+    if (angle1+angle2+angle3 == 180)
+    printf("The Pizza slice is Valid")
+    else
+    printf("The Pizza Slice is Not Valid");
+    return 0;
+}
